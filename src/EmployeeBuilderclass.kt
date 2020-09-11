4db65358502f8c1d0449dd336af2e8c151c2a192
haytham taha alsharany <talsharany@gmail.com>
2020-09-11 16:29:56 +0300

    second commit

---
+++ src/EmployeeBuilderclass.kt
@@ -0,0 +1,36 @@
+class Employee private constructor(var builder: Builder){
+    var  firstName:String=""
+     var  lastName:String=""
+    var id:String=""
+    var managerId:String=""
+    var isManager:Boolean=false
+    init {
+        this.firstName=builder.firstName
+        this.id=builder.id
+        this.isManager=builder.isManager
+        this.managerId=builder.managerId
+        this.lastName=builder.lastName
+    }
+
+    class  Builder{
+        var  firstName:String=""
+        var  lastName:String=""
+        var id:String=""
+        var managerId:String=""
+        var isManager:Boolean=false
+
+
+        fun build():Employee{
+            return Employee(this)
+
+        }
+
+        fun setFirstName(fName:String)=apply {this. firstName=fName }
+        fun setId(Id:String)=apply {this. id=Id }
+        fun setManagerId(managerId:String)=apply {this. managerId=managerId }
+        fun setIsManager(isManager:Boolean)=apply {this. isManager=isManager }
+        fun setLstName(lName:String)=apply {this. lastName=lName }
+
+    }
+
+}
